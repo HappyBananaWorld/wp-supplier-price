cfad3811f08df0aae6cd25e6d35c04e636a45f7d
Mr Banana <sajad.dev.personal@gmail.com>
2025-10-20 22:56:53 +0330

    add vendor autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'asaidea.ir/wp-supplier-price',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7ca57ae66f03de94011077a98db8a9227290266b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'asaidea.ir/wp-supplier-price' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7ca57ae66f03de94011077a98db8a9227290266b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
